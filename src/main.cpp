351c12011b164a18ac178e1a3751024710ffea80
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-05-21 22:45:45 +0200

    EDT - init empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,75 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void init_glfw()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+GLFWwindow *init_window(int width, int height)
+{
+    GLFWwindow* window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+    glfwMakeContextCurrent(window);
+    return window;
+}
+
+bool init_glad()
+{
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }
+    return true;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int window_w = 800;
+    int window_h = 600;
+
+    // init GLFW
+    init_glfw();
+
+    // init GLFW window
+    GLFWwindow *window = init_window(window_w, window_h);
+    if (!window)
+        return 1;
+
+    // init GLAD
+    if (!init_glad())
+        return 1;
+
+    // tell the size of window to openGL
+    glViewport(0, 0, window_w, window_h);
+
+    // set resize callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // main loop
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // exit properly
+    glfwTerminate();
+
     return 0;
 }

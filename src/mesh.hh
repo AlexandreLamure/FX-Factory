d8b8c9ed3f98bcfb1a135e1476a2dfb4fca13e65
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-05-22 18:26:01 +0200

    ADD - mesh class

---
+++ src/mesh.hh
@@ -0,0 +1,42 @@
+//
+// Created by alexandre on 22/05/19.
+//
+
+#ifndef OPENGL_GLITCH_MESH_HH
+#define OPENGL_GLITCH_MESH_HH
+
+#include <iostream>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader shader);
+
+private:
+    unsigned int VAO, VBO, EBO;
+
+    void setupMesh();
+};
+
+
+#endif //OPENGL_GLITCH_MESH_HH

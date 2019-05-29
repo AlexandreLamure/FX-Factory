4dccf9749736ecc336506a7c215220f2bfea0b8f
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-05-29 11:59:58 +0200

    FIX - moved the light computation from vertex to fragment shader

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -2,15 +2,28 @@
 
 in vec4 interpolated_color;
 in vec2 interpolated_tex_coords;
+in vec4 interpolated_pos;
+in vec4 interpolated_normal;
 
 out vec4 output_color;
 
 uniform float total_time;
 uniform sampler2D texture_diffuse1;
 
+uniform vec3 ambient_light_color;
+uniform vec3 diffuse_light_color;
+uniform vec3 diffuse_light_position;
+
 void main()
 {
+    // light computation
+    vec3 diffuse_light_dir = vec3(diffuse_light_position - interpolated_pos.xyz);
+    float coef = dot(normalize(interpolated_normal.xyz), normalize(diffuse_light_dir));
+    coef = clamp(coef, 0, 1);
+    vec4 light_color = vec4(diffuse_light_color * coef + ambient_light_color, 1.);
+
+
     //output_color = vec4(cos(2*total_time) / 2., sin(total_time) / 2., 0.2f, 1);
     vec4 texel = texture(texture_diffuse1, interpolated_tex_coords);
-    output_color = interpolated_color * texel;
+    output_color = light_color * texel;
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -4,6 +4,8 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 tex_coords;
 
+out vec4 interpolated_pos;
+out vec4 interpolated_normal;
 out vec4 interpolated_color;
 out vec2 interpolated_tex_coords;
 
@@ -12,23 +14,14 @@ uniform mat4 view;
 uniform mat4 projection;
 uniform mat4 normal_mat;
 
-uniform vec3 ambient_light_color;
-uniform vec3 diffuse_light_color;
-uniform vec3 diffuse_light_position;
-
 
 void main()
 {
-    vec4 new_pos = model * vec4(position, 1);
-    vec4 new_normal = normal_mat * vec4(normal, 1);
+    interpolated_pos = model * vec4(position, 1);
+    interpolated_normal = normal_mat * vec4(normal, 1);
 
-    // light computation
-    vec3 diffuse_light_dir = vec3(diffuse_light_position - new_pos.xyz);
-	float coef = dot(normalize(new_normal.xyz), normalize(diffuse_light_dir));
-	coef = clamp(coef, 0, 1);
-	interpolated_color = vec4(diffuse_light_color * coef + ambient_light_color, 1.);
 
-    gl_Position = projection * view * new_pos;
+    gl_Position = projection * view * interpolated_pos;
 
     interpolated_tex_coords = tex_coords;
 }

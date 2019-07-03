fe32a8f7a9d5d3f7af5db6097616a0c77df0e931
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-04 01:50:21 +0200

    EDT - simplified basic fragment shader

--- shaders/fragment/basic.glsl
+++ shaders/fragment/basic.glsl
@@ -16,22 +16,22 @@ uniform vec3 light2_color;
 uniform vec3 light2_position;
 uniform vec3 camera_pos;
 
-vec3 compute_lights(vec4 interpolated_pos, vec3 interpolated_normal,
-                    vec3 camera_pos,
+vec4 compute_lights(vec4 interpolated_pos, vec3 interpolated_normal,
                     vec3 ambient_light_color,
                     vec3 light1_color, vec3 light1_position,
-                    vec3 light2_color, vec3 light2_position);
+                    vec3 light2_color, vec3 light2_position,
+                    vec3 camera_pos,
+                    vec4 color_org);
+
 
 void main()
 {
-    vec3 light_color = compute_lights(interpolated_pos, interpolated_normal,
-                                      camera_pos,
-                                      ambient_light_color,
-                                      light1_color, light1_position,
-                                      light2_color, light2_position);
+    output_color = texture(texture_diffuse1, interpolated_tex_coords);
 
-
-    vec4 texel = texture(texture_diffuse1, interpolated_tex_coords);
-
-    output_color = vec4(light_color, 1) * texel;
+    output_color = compute_lights(interpolated_pos, interpolated_normal,
+                                  ambient_light_color,
+                                  light1_color, light1_position,
+                                  light2_color, light2_position,
+                                  camera_pos,
+                                  output_color);
 }

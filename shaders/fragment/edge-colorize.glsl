f7fb187020a2bc9b2708ad982747c2c08e2ecce5
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-03 12:17:15 +0200

    FIX - adapt edge-colorize to classic fragment shader instead of screen postprocessing

--- shaders/fragment/edge-colorize.glsl
+++ shaders/fragment/edge-colorize.glsl
@@ -1,20 +1,39 @@
 #version 430
 
+in vec4 interpolated_pos;
+in vec3 interpolated_normal;
+in vec4 interpolated_color;
 in vec2 interpolated_tex_coords;
 
 out vec4 output_color;
 
-uniform sampler2D screen_texture;
+uniform sampler2D texture_diffuse1;
 uniform float total_time;
-uniform vec2 mouse_pos;
+
+uniform vec3 ambient_light_color;
+uniform vec3 light1_color;
+uniform vec3 light1_position;
+uniform vec3 light2_color;
+uniform vec3 light2_position;
+uniform vec3 camera_pos;
+uniform int mesh_id;
 uniform int rand;
+
+
+vec3 compute_lights(vec4 interpolated_pos, vec3 interpolated_normal,
+                    vec3 camera_pos,
+                    vec3 ambient_light_color,
+                    vec3 light1_color, vec3 light1_position,
+                    vec3 light2_color, vec3 light2_position);
+
+
 
 #define EDGE_THRESHOLD 0.55
 #define PI 3.1415926535
 
 float is_edge(vec2 coords)
 {
-    vec2 screen_size = textureSize(screen_texture, 0);
+    vec2 screen_size = textureSize(texture_diffuse1, 0);
     float dx = 1.0 / float(screen_size.x);
     float dy = 1.0 / float(screen_size.y);
     float kernel[9] = float[](0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -27,7 +46,7 @@ float is_edge(vec2 coords)
         for(int j = -1; j < 2; j++)
         {
             k++;
-            vec4 tex_color = texture(screen_texture, coords + vec2(float(i)*dx, float(j)*dy));
+            vec4 tex_color = texture(texture_diffuse1, coords + vec2(float(i)*dx, float(j)*dy));
             kernel[k] = (tex_color.r + tex_color.g + tex_color.b) / 3.;
         }
     }
@@ -44,14 +63,19 @@ float is_edge(vec2 coords)
 
 void main()
 {
+    vec3 light_color = compute_lights(interpolated_pos, interpolated_normal,
+                                        camera_pos,
+                                        ambient_light_color,
+                                        light1_color, light1_position,
+                                        light2_color, light2_position);
     vec2 uv = interpolated_tex_coords.xy;
 
-    vec3 tex_color = texture(screen_texture, uv).rgb;
+    vec3 tex_color = texture(texture_diffuse1, uv).rgb;
 
     vec3 edge_color = vec3(0.0, 0.0, 0.0);
     edge_color.r = cos(uv.x * tex_color.r) * sin(total_time);
     edge_color.g = edge_color.r * cos(total_time * uv.y);
     edge_color.r = edge_color.r / edge_color.g * cos(uv.y) * sin(total_time);
 
-    output_color = vec4((is_edge(uv) >= EDGE_THRESHOLD) ? edge_color : tex_color, 1);
+    output_color = vec4(light_color * ((is_edge(uv) >= EDGE_THRESHOLD) ? edge_color : tex_color), 1);
 }

e6bd2e7b79965d660f9071576d3d253e75113abf
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-04 01:45:12 +0200

    EDT - transformed edge into a lib shader

--- shaders/fragment/edge.glsl
+++ shaders/fragment/edge.glsl
@@ -1,81 +1,52 @@
 #version 430
 
-in vec4 interpolated_pos;
-in vec3 interpolated_normal;
-in vec4 interpolated_color;
-in vec2 interpolated_tex_coords;
-
-out vec4 output_color;
-
-uniform sampler2D texture_diffuse1;
-uniform float total_time;
-
-uniform vec3 ambient_light_color;
-uniform vec3 light1_color;
-uniform vec3 light1_position;
-uniform vec3 light2_color;
-uniform vec3 light2_position;
-uniform vec3 camera_pos;
-uniform int mesh_id;
-uniform int rand;
-
-
-vec3 compute_lights(vec4 interpolated_pos, vec3 interpolated_normal,
-                    vec3 camera_pos,
-                    vec3 ambient_light_color,
-                    vec3 light1_color, vec3 light1_position,
-                    vec3 light2_color, vec3 light2_position);
-
-
-
 #define EDGE_THRESHOLD 0.55
-#define PI 3.1415926535
 
-float is_edge(vec2 coords)
+float is_edge(vec2 uv, sampler2D texture_diffuse1,
+              float total_time)
 {
-    vec2 screen_size = textureSize(texture_diffuse1, 0);
-    float dx = 1.0 / float(screen_size.x);
-    float dy = 1.0 / float(screen_size.y);
+    vec2 tex_size = textureSize(texture_diffuse1, 0);
+    float dx = 1.0 / float(tex_size.x*4) * cos(total_time / 2) * 5;
+    float dy = 1.0 / float(tex_size.y*4) * sin(total_time / 2) * 5;
     float kernel[9] = float[](0, 0, 0, 0, 0, 0, 0, 0, 0);
     int k = -1;
     float delta;
 
     // read neighboring pixel intensities
-    for (int i = -1; i < 2; i++)
+    for (int y = -1; y < 2; y++)
     {
-        for(int j = -1; j < 2; j++)
+        for(int x = -1; x < 2; x++)
         {
             k++;
-            vec4 tex_color = texture(texture_diffuse1, coords + vec2(float(i)*dx, float(j)*dy));
+            vec4 tex_color = texture(texture_diffuse1, uv + vec2(float(x)*dx, float(y)*dy));
             kernel[k] = (tex_color.r + tex_color.g + tex_color.b) / 3.;
         }
     }
 
-    // average color differences around neighboring pixels
-    delta = (abs(kernel[1]-kernel[7])
-    + abs(kernel[5]-kernel[3])
-    + abs(kernel[0]-kernel[8])
-    + abs(kernel[2]-kernel[6])
-    ) / 4.;
+    // average color of opposite neighbors difference
+    delta = (abs(kernel[0]-kernel[8])
+             + abs(kernel[3]-kernel[5])
+             + abs(kernel[6]-kernel[2])
+             + abs(kernel[7]-kernel[1])
+             ) / 4.;
 
     return clamp(5.0 * delta, 0.0, 1.0);
 }
 
-void main()
+vec4 edge_enhance(vec2 uv,
+                  sampler2D texture_diffuse1,
+                  float total_time,
+                  vec4 color_org, float edge_threshold, bool colorize)
 {
-    vec3 light_color = compute_lights(interpolated_pos, interpolated_normal,
-                                        camera_pos,
-                                        ambient_light_color,
-                                        light1_color, light1_position,
-                                        light2_color, light2_position);
-    vec2 uv = interpolated_tex_coords.xy;
-
-    vec3 tex_color = texture(texture_diffuse1, uv).rgb;
-
     vec3 edge_color = vec3(0.0, 0.0, 0.0);
-    edge_color.r = cos(uv.x * tex_color.r) * sin(total_time);
-    edge_color.g = edge_color.r * cos(total_time * uv.y);
-    edge_color.r = edge_color.r / edge_color.g * cos(uv.y) * sin(total_time);
-
-    output_color = vec4(light_color * ((is_edge(uv) >= EDGE_THRESHOLD) ? edge_color : tex_color), 1);
+    if (colorize)
+    {
+        edge_color.r = cos(uv.x * color_org.r) * sin(total_time);
+        edge_color.g = edge_color.r * cos(total_time * uv.y);
+        edge_color.r = edge_color.r / edge_color.g * cos(uv.y) * sin(total_time);
+    }
+    if ((is_edge(uv, texture_diffuse1, total_time) >= edge_threshold))
+        return vec4(edge_color, color_org.a);
+    else
+        return color_org;
 }

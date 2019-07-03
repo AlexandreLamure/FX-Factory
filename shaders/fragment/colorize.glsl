daa05e9e07c7c04ed0d129f20b9b5a8213c73f93
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-03 17:58:01 +0200

    EDT - simplified colorize shader

--- shaders/fragment/colorize.glsl
+++ shaders/fragment/colorize.glsl
@@ -19,49 +19,20 @@ uniform vec3 camera_pos;
 uniform int mesh_id;
 uniform int rand;
 
-bool glitch_trigger(float rate, float param1, float param2, float param3)
-{
-    return ((cos(total_time * rate) * sin(rand * rate * param1 / param2) * sin(param2 * param3) + (fract(param1 * rand) * rand + cos(param2 * param3 / param1) * rand) / (rand * int(total_time) % 10))
-           + float(int(param1 * param2 * param3 + (rand / 20)) % 100) / 100 > 0.)
-            && cos(total_time * rate) > 0.5
-            && abs(int(sin(total_time * rate) * 20)) == rand % 20;
-}
+vec3 compute_lights(vec4 interpolated_pos, vec3 interpolated_normal,
+                    vec3 camera_pos,
+                    vec3 ambient_light_color,
+                    vec3 light1_color, vec3 light1_position,
+                    vec3 light2_color, vec3 light2_position);
+
 
 void main()
 {
-    // Light Computation
-    vec3 normal = normalize(interpolated_normal);
-
-    // diffuse 1
-    vec3 light1_dir = normalize(vec3(light1_position - interpolated_pos.xyz));
-    float coef = dot(normal, light1_dir);
-    coef = clamp(coef, 0, 1);
-    vec3 light_color = light1_color * coef;
-
-    // diffuse 2
-    vec3 light2_dir = normalize(vec3(light2_position - interpolated_pos.xyz));
-    float coef2 = dot(normal, light2_dir);
-    coef2 = clamp(coef2, 0, 1);
-    light_color += light2_color * coef2;
-
-    // specular 1
-    int shininess = 32;
-    float spec_strength = 0.7;
-    vec3 camera_dir1 = normalize(camera_pos - interpolated_pos.xyz);
-    vec3 reflect_dir1 = reflect(-light1_dir, normal);
-    float spec1 = pow(max(dot(camera_dir1, reflect_dir1), 0.0), shininess);
-    vec3 specular1 = spec_strength * spec1 * light1_color;
-    light_color += specular1;
-
-    // specular 2
-    vec3 camera_dir2 = normalize(camera_pos - interpolated_pos.xyz);
-    vec3 reflect_dir2 = reflect(-light2_dir, normal);
-    float spec2 = pow(max(dot(camera_dir2, reflect_dir2), 0.0), shininess);
-    vec3 specular2 = spec_strength * spec2 * light2_color;
-    light_color += specular2;
-
-    // ambient
-    light_color += ambient_light_color;
+    vec3 light_color = compute_lights(interpolated_pos, interpolated_normal,
+                                        camera_pos,
+                                        ambient_light_color,
+                                        light1_color, light1_position,
+                                        light2_color, light2_position);
 
 
     vec4 texel = texture(texture_diffuse1, interpolated_tex_coords);
@@ -103,16 +74,4 @@ void main()
     output_color += glitch_color;
     output_color -= glitch_color2;
     output_color -= glitch_color3;
-
-
-    /*
-    vec4 glitch_color4 = vec4(0);
-    if (glitch_trigger(20, mesh_id, interpolated_pos.x, interpolated_normal.y))
-    {
-        glitch_color4.g = cos(total_time);
-        glitch_color4.r += 1;
-        glitch_color4.b *= fract(total_time);
-    }
-    output_color = vec4(light_color, 1) * texel;
-    output_color += glitch_color4;*/
 }

9b37c269031898489defee6dc8178cd0f79cf246
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-05 14:50:30 +0200

    EDT - simplified old-video screen shader

--- shaders/fragment/screen/old-video.glsl
+++ shaders/fragment/screen/old-video.glsl
@@ -3,9 +3,8 @@
 //#define BLACK_AND_WHITE  // FIXME: make it a line that goes down the screen
 #define LINES_AND_FLICKER
 #define BLOTCHES
-#define GRAIN
-
-#define FREQUENCY 15.0
+//#define GRAIN
+//#define DESCENT
 
 in vec2 interpolated_tex_coords;
 
@@ -17,117 +16,116 @@ uniform float total_time;
 uniform float delta_time;
 uniform int rand;
 
-vec2 uv;
-
-float randf(vec2 co){
-    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-}
-
-float randf(float c){
-    return randf(vec2(c,1.0));
-}
-
-float randomLine(float seed)
+float randf(vec2 seed)
 {
-    float b = 0.01 * randf(seed);
-    float a = randf(seed+1.0);
-    float c = randf(seed+2.0) - 0.5;
-    float mu = randf(seed+3.0);
+    return fract(sin(dot(seed.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
 
-    float l = 1.0;
+float randf(float seed)
+{
+    return randf(vec2(seed,1.0));
+}
 
-    if ( mu > 0.2)
-    l = pow(  abs(a * uv.x + b * uv.y + c ), 1.0/8.0 );
-    else
-    l = 2.0 - pow( abs(a * uv.x + b * uv.y + c), 1.0/8.0 );
+float random_line(vec2 uv, float seed)
+{
+    float a = randf(seed);
+    float b = randf(seed + 1) * 0.01;
+    float c = randf(seed + 2) - 0.5;
+    float mu = randf(seed + 3);
+
+    float l = pow(abs(a * uv.x + b * uv.y + c), 0.125);
+    if (mu > 0.2)
+        l = 2 - l;
 
     return mix(0.5, 1.0, l);
 }
 
 // Generate some blotches.
-float randomBlotch(float seed)
+float random_blotch(vec2 uv, float seed)
 {
     float x = randf(seed);
-    float y = randf(seed+1.0);
-    float s = 0.01 * randf(seed+2.0);
+    float y = randf(seed + 1);
+    float s = randf(seed + 2) * 0.01;
 
-    vec2 p = vec2(x,y) - uv;
+    vec2 p = vec2(x, y) - uv;
     p.x *= resolution.x / resolution.y;
-    float a = atan(p.y,p.x);
-    float v = 1.0;
-    float ss = s*s * (sin(6.2831*a*x)*0.1 + 1.0);
+    float a = atan(p.y, p.x);
+    float v = 0.2;
+    float ss = s * s * (sin(6.2831 * a * x) + 100.0);
 
-    if ( dot(p,p) < ss ) v = 0.2;
-    else
-    v = pow(dot(p,p) - ss, 1.0/16.0);
+    if (dot(p, p) > ss)
+        v = pow(dot(p, p) - ss, 0.0625);
 
     return mix(0.3 + 0.2 * (1.0 - (s / 0.02)), 1.0, v);
 }
 
 void main()
 {
-    uv = interpolated_tex_coords;//interpolated_pos.xy / resolution.xy;
+    vec2 uv = interpolated_tex_coords;
 
-    // Set frequency of global effect to 20 variations per second
-    float t = interpolated_tex_coords.y * rand;//float(int(total_time * FREQUENCY));
+    float intensity = interpolated_tex_coords.y * rand;
 
     // Get some image movement
-    vec2 suv = uv + 0.002 * vec2( randf(t), randf(t + 23.0));
+    vec2 suv = uv + 0.002 * vec2( randf(intensity), randf(intensity + 23.0));
 
     // Get the image
-    vec3 image = texture2D( screen_texture, vec2(suv.x, suv.y) ).xyz;
+    vec4 tex_color = texture2D(screen_texture, vec2(suv.x, suv.y));
+
+
+
+    #ifdef DESCENT
+    // descending line
+    float line_pos = 1.0 - (total_time / 10 - floor(total_time / 10));
+    line_pos = line_pos * 5 - 4;
+    float line_height = 0.01 + uv.x * rand / 10000;
+    if (interpolated_tex_coords.y > line_pos - line_height && interpolated_tex_coords.y < line_pos)
+    {
+        tex_color = texture(screen_texture, interpolated_tex_coords - 0.1 * rand / 1000);
+        if (cos(total_time) * sin(total_time) * cos(total_time) < 0)
+        {
+            float luma_line = 0.2126 * tex_color.r + 0.7152 * tex_color.g + 0.0722 * tex_color.b;
+            output_color.rgb = mix(vec3(luma_line), tex_color.rgb, 0.15);
+        }
+        else
+        output_color = tex_color;
+    }
+    #endif
+
+
 
     #ifdef BLACK_AND_WHITE
-    // Pass it to B/W
-    float luma = dot( vec3(0.2126, 0.7152, 0.0722), image );
-    vec3 oldImage = luma * vec3(0.7, 0.7, 0.7);
-    #else
-    vec3 oldImage = image;
+    float luma = 0.2126 * tex_color.r + 0.7152 * tex_color.g + 0.0722 * tex_color.b;
+    tex_color.rgb = mix(vec3(luma), tex_color.rgb, 0.25);
     #endif
 
     // Create a total_time-varyting vignetting effect
-    float vI = 16.0 * (uv.x * (1.0-uv.x) * uv.y * (1.0-uv.y));
-    vI *= mix( 0.7, 1.0, randf(t + 0.5));
+    float glitch_coef = 10 * (uv.x * (1.0-uv.x) * uv.y * (1.0-uv.y));
+    glitch_coef *= mix(0.7, 1.0, randf(intensity + 0.5));
 
     // Add additive flicker
-    vI += 1.0 + 0.4 * randf(t+8.);
+    glitch_coef += 0.7 + 0.04 * randf(intensity + 8);
 
     // Add a fixed vignetting (independent of the flicker)
-    vI *= pow(16.0 * uv.x * (1.0-uv.x) * uv.y * (1.0-uv.y), 0.4);
+    glitch_coef *= pow(20 * uv.x * (1.0-uv.x) * uv.y * (1.0-uv.y), 0.4);
 
     // Add some random lines (and some multiplicative flicker. Oh well.)
     #ifdef LINES_AND_FLICKER
-    int l = int(8.0 * randf(t+7.0));
-
-    if ( 0 < l ) vI *= randomLine( t);
-    if ( 1 < l ) vI *= randomLine( t);
-    if ( 2 < l ) vI *= randomLine( t);
-    if ( 3 < l ) vI *= randomLine( t);
-    if ( 4 < l ) vI *= randomLine( t);
-    if ( 5 < l ) vI *= randomLine( t);
-    if ( 6 < l ) vI *= randomLine( t);
-    if ( 7 < l ) vI *= randomLine( t);
-
+    int intensity_laf = int(3.5 * randf(intensity + 7.0));
+    for (int i = 0; i < intensity_laf; ++i)
+        glitch_coef *= random_line(uv, intensity);
     #endif
 
     // Add some random blotches.
     #ifdef BLOTCHES
-    int s = int( max(8.0 * randf(t+18.0) -2.0, 0.0 ));
-
-    if ( 0 < s ) vI *= randomBlotch( t);
-    if ( 1 < s ) vI *= randomBlotch( t);
-    if ( 2 < s ) vI *= randomBlotch( t);
-    if ( 3 < s ) vI *= randomBlotch( t);
-    if ( 4 < s ) vI *= randomBlotch( t);
-    if ( 5 < s ) vI *= randomBlotch( t);
-
+    int intensity_blotch = int(max(5.0 * randf(intensity+18.0) -2.0, 0.0));
+    for (int i = 0; i < intensity_blotch; ++i)
+        glitch_coef *= random_blotch(uv, intensity);
     #endif
 
-    // Show the image modulated by the defects
-    output_color.xyz = oldImage * vI;
+    output_color = vec4(tex_color.rgb * glitch_coef, tex_color.a);
 
-    // Add some grain (thanks, Jose!)
     #ifdef GRAIN
-    output_color.xyz *= (1.0+(randf(uv+t*.01)-.2)*.15);
+    output_color.xyz *= (0.9 + (randf(uv + float(rand) / 1000)) * 0.45);
     #endif
+
 }

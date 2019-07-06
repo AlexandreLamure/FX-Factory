5ffb1c2a1851d1a50d79485eadb1076620d0699c
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-06 19:01:52 +0200

    EDT - exported random funcs in lib

---
+++ shaders/random.glsl
@@ -0,0 +1,18 @@
+#version 430
+
+float randf(vec2 seed)
+{
+    return fract(sin(dot(seed.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+float randf(float seed)
+{
+    return randf(vec2(seed,1.0));
+}
+
+// returns a float in [0, 1[ that is jerky
+float jerky_rand(float seed)
+{
+    float duration = 10;
+    return floor(fract(seed) * duration) / duration;
+}

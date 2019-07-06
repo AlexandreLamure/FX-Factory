51498260a01aca5ecb7ad6a56553dd737cffe980
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-06 19:00:48 +0200

    EDT - transformed distortion to 'lib' shader

--- shaders/fragment/screen/distortion.glsl
+++ shaders/fragment/screen/distortion.glsl
@@ -1,24 +1,15 @@
 #version 430
 
-in vec2 interpolated_tex_coords;
-
-out vec4 output_color;
-
-uniform sampler2D screen_texture;
-uniform float total_time;
-uniform vec2 mouse_pos;
-uniform int rand;
-
-
-void main()
+vec4 distortion(vec2 uv,
+                sampler2D screen_texture,
+                float total_time,
+                int rand)
 {
-    vec2 dir = interpolated_tex_coords - vec2(.5);
+    vec2 dir = uv - vec2(.5);
     float d = .7 * length(dir) * total_time / 10;
     normalize(dir);
     vec2 value = d * dir;
-    vec2 glitch_tex_coords = vec2(interpolated_tex_coords.x + sin(interpolated_tex_coords.x) * d,
-                                  interpolated_tex_coords.y + sin(interpolated_tex_coords.x) * d);
-    vec4 texel = texture2D(screen_texture, glitch_tex_coords);
-
-    output_color = texel;
+    vec2 glitch_uv = vec2(uv.x + sin(uv.x) * d,
+                          uv.y + sin(uv.x) * d);
+    return texture2D(screen_texture, glitch_uv);
 }

dfebf4f1dc284c21bc53e087ae96349f479f3fb2
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-07 08:50:18 +0200

    EDT - transformed tex-transpose vertex shader to 'lib' shader

--- shaders/vertex/tex-transpose.glsl
+++ shaders/vertex/tex-transpose.glsl
@@ -1,27 +1,9 @@
 #version 430
 
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 normal;
-layout (location = 2) in vec2 tex_coords;
-
-out vec4 interpolated_pos;
-out vec3 interpolated_normal;
-out vec4 interpolated_color;
-out vec2 interpolated_tex_coords;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-uniform int mesh_id;
-uniform float total_time;
-uniform int rand;
-
-
-
-void main()
+vec3 tex_transpose(vec3 position, float total_time, int mesh_id, int rand, float glitch_intensity)
 {
     vec3 position_glitch = position;
-    float glitch_intensity = 0.002;
+
     if (rand % 50 == int(total_time) % 50)
     {
         if (int(10*position.y) % 3 == 0)
@@ -37,11 +19,6 @@ void main()
     {
         position_glitch *= 1.08;
     }
-    interpolated_pos = model * vec4(position_glitch, 1);
-    interpolated_normal = mat3(transpose(inverse(model))) * normal; // we only keep the scale and rotations from model matrix
 
-
-    gl_Position = projection * view * interpolated_pos;
-
-    interpolated_tex_coords = tex_coords;
+    return position_glitch;
 }

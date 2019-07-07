a32501be888a794afa4b6e5f2aaca8b9eed316fd
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-07-07 08:50:53 +0200

    EDT - add vertex shader flags and tex_id_glitch to fx_factory

--- src/fx-factory.cc
+++ src/fx-factory.cc
@@ -7,13 +7,16 @@
 
 namespace FX
 {
-    FXFactory::FXFactory(int nb_models) {
+    FXFactory::FXFactory(int nb_models)
+    {
+        vertex_renders = std::vector<VertexRender>(nb_models);
+        frag_renders = std::vector<FragRender>(nb_models);
 
-        frag_renders = std::vector<FragRender >(nb_models);
         for (int i = 0; i < nb_models; ++i)
             frag_renders[i] = FragRender::COMPUTE_LIGHT | FragRender::TEX_BEFORE;
 
         frag_screen = FragScreen::SCREEN_TEX_BEFORE;
         current_model = 0;
+        tex_id_glitch = 0;
     }
 }
--- src/fx-factory.hh
+++ src/fx-factory.hh
@@ -7,27 +7,35 @@
 
 #include <vector>
 
-namespace FX {
-    enum FragRender {
-        UNDEFINED = 1 << 0,
-        COMPUTE_LIGHT = 1 << 1,
-        TEX_BEFORE = 1 << 2,
-        TEX_MOVE = 1 << 3,
-        TEX_MOVE_GLITCH = 1 << 4,
-        COLORIZE = 1 << 5,
-        TEX_RGB_SPLIT = 1 << 6,
-        EDGE_ENHANCE = 1 << 7,
-        TOONIFY = 1 << 8,
-        HORRORIFY = 1 << 9
+namespace FX
+{
+    enum VertexRender
+    {
+        TEX_TRANSPOSE           = 1 << 0
     };
 
-    enum FragScreen {
-        SCREEN_UNDEFINED = 1 << 0,
-        SCREEN_TEX_BEFORE = 1 << 1,
-        SCREEN_TEX_RGB_SPLIT = 1 << 2,
-        SCREEN_RECTANGLES = 1 << 3,
-        SCREEN_DISTORTION = 1 << 4,
-        SCREEN_K7 = 1 << 5
+    enum FragRender
+    {
+        UNDEFINED               = 1 << 0,
+        COMPUTE_LIGHT           = 1 << 1,
+        TEX_BEFORE              = 1 << 2,
+        TEX_MOVE                = 1 << 3,
+        TEX_MOVE_GLITCH         = 1 << 4,
+        COLORIZE                = 1 << 5,
+        TEX_RGB_SPLIT           = 1 << 6,
+        EDGE_ENHANCE            = 1 << 7,
+        TOONIFY                 = 1 << 8,
+        HORRORIFY               = 1 << 9
+    };
+
+    enum FragScreen
+    {
+        SCREEN_UNDEFINED        = 1 << 0,
+        SCREEN_TEX_BEFORE       = 1 << 1,
+        SCREEN_TEX_RGB_SPLIT    = 1 << 2,
+        SCREEN_RECTANGLES       = 1 << 3,
+        SCREEN_DISTORTION       = 1 << 4,
+        SCREEN_K7               = 1 << 5
     };
 
     template<typename T>
@@ -53,10 +61,11 @@ namespace FX {
     class FXFactory
     {
     public:
-        //std::vector<VertexRender> vertex_renders;
+        std::vector<VertexRender> vertex_renders;
         std::vector<FragRender> frag_renders;
         FragScreen frag_screen;
         int current_model;
+        int tex_id_glitch;
 
         FXFactory()
         {};
